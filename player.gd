e69591a31427ab8b330f2820a33cd560387df565
ubermensch04 <sparsh.sg04@gmail.com>
2024-06-04 12:25:42 +0530

    basic controller, ray cast error exists

--- player.gd
+++ player.gd
@@ -1,15 +1,22 @@
 extends CharacterBody3D
 
 @onready var head = $Head
+@onready var standing_collision_shape = $StandingCollisionShape
+@onready var crouching_collision_shape = $CrouchingCollisionShape
+@onready var ray_cast_3d = $RayCast3D
 
 var current_speed = 5.0
 const walking_speed= 5.0
-const sprinting_speed=8.0
+const sprinting_speed=9.0
 const crouching_speed=3.0
 const jump_velocity = 4.5
 
-const mouse_sens=0.4
+const mouse_sens=0.25
+var direction=Vector3.ZERO
 
+var lerp_speed=7.5
+
+var crouching_depth=-0.5
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
@@ -19,12 +26,23 @@ func _ready():
 func _input(event):
 	if event is InputEventMouseMotion:
 		rotate_y(-deg_to_rad(event.relative.x*mouse_sens))
+		head.rotate_x(-deg_to_rad(event.relative.y*mouse_sens))
+		head.rotation.x=clamp(head.rotation.x,deg_to_rad(-89),deg_to_rad(89))
 
 func _physics_process(delta):
-	if Input.is_action_pressed("sprint"):
-		current_speed=sprinting_speed
-	else:
-		current_speed=walking_speed
+	if Input.is_action_pressed("crouch"):
+		current_speed=crouching_speed
+		head.position.y=lerp(head.position.y, 1.8 + crouching_depth,delta*lerp_speed)
+		standing_collision_shape.disabled=true
+		crouching_collision_shape.disabled=false
+	elif ray_cast_3d.is_colliding():
+		standing_collision_shape.disabled=false
+		crouching_collision_shape.disabled=true
+		head.position.y=lerp(head.position.y, 1.8,delta*lerp_speed)
+		if Input.is_action_pressed("sprint"):
+			current_speed=sprinting_speed
+		else:
+			current_speed=walking_speed
 		
 	# Add the gravity.
 	if not is_on_floor():
@@ -37,7 +55,7 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	direction = lerp(direction,(transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized(),delta*lerp_speed)
 	if direction:
 		velocity.x = direction.x * current_speed
 		velocity.z = direction.z * current_speed
