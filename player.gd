8d0635ac7eb41de350aeb54ccad8a9296de8aa90
ubermensch04 <sparsh.sg04@gmail.com>
2024-06-04 14:59:09 +0530

    pre ray-cast optimization

--- player.gd
+++ player.gd
@@ -3,20 +3,18 @@ extends CharacterBody3D
 @onready var head = $Head
 @onready var standing_collision_shape = $StandingCollisionShape
 @onready var crouching_collision_shape = $CrouchingCollisionShape
-@onready var ray_cast_3d = $RayCast3D
 
 var current_speed = 5.0
 const walking_speed= 5.0
-const sprinting_speed=9.0
+const sprinting_speed=8.0
 const crouching_speed=3.0
 const jump_velocity = 4.5
 
-const mouse_sens=0.25
+const mouse_sens=0.4
+var lerp_speed= 15
+
 var direction=Vector3.ZERO
-
-var lerp_speed=7.5
-
-var crouching_depth=-0.5
+var crouching_depth = -0.5
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
@@ -28,17 +26,17 @@ func _input(event):
 		rotate_y(-deg_to_rad(event.relative.x*mouse_sens))
 		head.rotate_x(-deg_to_rad(event.relative.y*mouse_sens))
 		head.rotation.x=clamp(head.rotation.x,deg_to_rad(-89),deg_to_rad(89))
-
+		
 func _physics_process(delta):
 	if Input.is_action_pressed("crouch"):
-		current_speed=crouching_speed
-		head.position.y=lerp(head.position.y, 1.8 + crouching_depth,delta*lerp_speed)
 		standing_collision_shape.disabled=true
 		crouching_collision_shape.disabled=false
-	elif ray_cast_3d.is_colliding():
+		current_speed=crouching_speed
+		head.position.y=lerp(head.position.y,1.8+crouching_depth,delta*lerp_speed)
+	else:
 		standing_collision_shape.disabled=false
 		crouching_collision_shape.disabled=true
-		head.position.y=lerp(head.position.y, 1.8,delta*lerp_speed)
+		head.position.y=lerp(head.position.y,1.8,delta*lerp_speed)
 		if Input.is_action_pressed("sprint"):
 			current_speed=sprinting_speed
 		else:
